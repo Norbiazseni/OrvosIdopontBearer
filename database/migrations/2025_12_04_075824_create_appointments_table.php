2c72b89c6c9a2127d0374e180668d3ed0237ee85
Norbiazseni <131431630+Norbiazseni@users.noreply.github.com>
2025-12-10 21:42:00 +0000

    softdelete

--- database/migrations/2025_12_04_075824_create_appointments_table.php
+++ database/migrations/2025_12_04_075824_create_appointments_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 return new class extends Migration
 {
@@ -26,6 +27,7 @@ return new class extends Migration
             $table->string('status')->default('scheduled');
 
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
