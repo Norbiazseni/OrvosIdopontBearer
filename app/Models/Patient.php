51bdac5fbe141cf85274eae10b98785a6ebbb8bb
Norbiazseni <131431630+Norbiazseni@users.noreply.github.com>
2025-12-10 21:36:47 +0000

    Enable soft deletes for Patient model

--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -4,10 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Patient extends Model
 {
     use HasFactory; // EZ FONTOS
+    use SoftDeletes;
 
     protected $fillable = ['name', 'email', 'birth_date'];
 }
